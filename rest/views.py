c180f6a86385fc8eb1e2d6e67a7ccf5683727b82
aayushker <singhaayushker@gmail.com>
2024-06-29 18:37:35 +0530

    Completed Create Operation

--- rest/views.py
+++ rest/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Employees
 
 # Create your views here.
+
+def home(request):
+    emp = Employees.objects.all()
+    context = {
+        'emp': emp
+
+    }
+    return render(request, 'rest/index.html', context)
